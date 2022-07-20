4023b3ade68126038581fb90a36990bdde364252
MrKaydee <44779363+koredebishi@users.noreply.github.com>
2022-07-20 22:02:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+# def greet():
+#   print(f"Welcom to the greet function")
+#   print(f"Hope your journey is smooth")
+#   print(f"I am happy to have you hear")
+
+# greet()
+
+# def greet(name):
+#   print(f"Welcom to the greet function")
+#   print(f"Hope your journey is smooth")
+# name = str(input(f"Enter ur name\n"))
+# print(f"I am happy to have you hear\n{name}")
+# greet(name)
+
+# def bitcoin_to_usd(btc):
+#     amount = btc * 527
+#     print(amount)
+# btc = input("Input your Bitcoins amount: ")
+# bitcoin_to_usd(btc)
+
+def greet (name):
+  print("Welcom to the greet function", name)
+  print ("We are happy to have you hear ", name)
+
+greet("Korede")
